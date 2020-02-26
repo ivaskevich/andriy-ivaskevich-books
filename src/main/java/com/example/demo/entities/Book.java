4ea31cb1772d9513de9759103c3881b4ef0987ed
Andriy Ivaskevich <ivaskevich@users.noreply.github.com>
2020-02-26 07:02:18 +0200

    Added datasource dependencies

--- src/main/java/com/example/demo/entities/Book.java
+++ src/main/java/com/example/demo/entities/Book.java
@@ -4,10 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
     private String isbn;
     private String title;
     private String author;

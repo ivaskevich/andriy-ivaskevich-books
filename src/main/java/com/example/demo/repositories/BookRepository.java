8398a86835561add66e8f0c908285b6a1d8f63fd
Andriy Ivaskevich <ivaskevich@users.noreply.github.com>
2020-02-26 07:16:36 +0200

    Adding annotation

--- src/main/java/com/example/demo/repositories/BookRepository.java
+++ src/main/java/com/example/demo/repositories/BookRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repositories;
 import com.example.demo.entities.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
@@ -15,14 +16,17 @@ public class BookRepository {
 
     private final ArrayList<Book> books = new ArrayList<>();
 
+    @Transactional
     public Book addBook(Book book) {
         return entityManager.merge(book);
     }
 
+    @Transactional
     public Book findById(Integer id) {
-        return entityManager.find(Book.class,id);
+        return entityManager.find(Book.class, id);
     }
 
+    @Transactional
     public void deleteBook(Integer id) {
         if (id != null) {
             Book b = findById(id);
@@ -30,6 +34,7 @@ public class BookRepository {
         }
     }
 
+    @Transactional
     public ArrayList<Book> getBooks() {
         return new ArrayList<>(books);
     }
